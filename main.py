99ce4f29a4ed48b42641ed5745a06a8c16bbddde
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-06-02 12:56:58 -0300

    1.0

---
+++ main.py
@@ -0,0 +1,348 @@
+
+# Importando o módulo do sqlite3 para manipularmos o banco de dados SQLITE3
+import sqlite3
+# Importando o módulo do sqlite3 e a classe Error para exibir mensagens de erro
+from sqlite3 import Error
+# Importando o módulo time para utilizar o método sleep que aguarda o 
+# tempo em segundos informando dentro do parênteses
+from time import sleep
+# Importando o arquivo schema.py para criar as tabelas do banco de dados
+import schema
+# Importando o arquivo tabelas para manipular as informações nelas (CRUD)
+# Criar, atualizar, excluir, e pesquisar.
+import tabelas
+# Importando o o arquivo cores.py, a classe Cores para colocar cores nos textos
+from cores import Cores
+#Opções visualizadas pelo Admin do sistema
+def administrador():
+  print(33 * '*')
+  print(f"{Cores.BOLD}{Cores.OKBLUE}***-- Perfil Administrador --***{Cores.ENDC}")
+  print(33 * '*')
+  print(f"{Cores.BOLD}{Cores. OKGREEN}Tabela de escolha :{Cores.ENDC}")
+  print(f"{Cores.FAIL}1. Cadastrar Professor{Cores.ENDC}")
+  print(f"{Cores.FAIL}2. Alterar Professor{Cores.ENDC}")
+  print(f"{Cores.FAIL}3. Ver professores Cadastrados{Cores.ENDC}")
+  print(f"{Cores.FAIL}4. Excluir Cadastrados{Cores.ENDC}")  
+  print(33 * '*')
+  print(f"{Cores.FAIL}5. Cadastrar Alunos{Cores.ENDC}")
+  print(f"{Cores.FAIL}6. Alterar Alunos{Cores.ENDC}")
+  print(f"{Cores.FAIL}7. Ver Alunos Cadastrados{Cores.ENDC}")
+  print(f"{Cores.FAIL}8. Excluir Alunos{Cores.ENDC}")  
+  print(33 * '*')
+  print(f"{Cores.FAIL}9. Cadastrar tipo de prova{Cores.ENDC}")
+  print(f"{Cores.FAIL}10. Alterar tipo de prova{Cores.ENDC}")
+  print(f"{Cores.FAIL}11. Ver tipo de prova{Cores.ENDC}")
+  print(f"{Cores.FAIL}12. Excluir tipo de prova{Cores.ENDC}")    
+  print(33 * '*')
+  
+  print(f"{Cores.FAIL}0. Sair{Cores.ENDC}")
+  opcaoadm = int(input("X. Escolha uma opçao : "))
+  return opcaoadm
+
+#Opções visualizadas pelo aluno
+def aluno():
+	print(33 * '*')
+	print(f"{Cores.BOLD}{Cores.OKBLUE}***-- Perfil Aluno --***{Cores.ENDC}")
+	print(33 * '*')
+	print(f"{Cores.BOLD}{Cores. OKGREEN}Tabela de escolha :{Cores.ENDC}")
+	print(f"{Cores.FAIL}1. Visualizar notas{Cores.ENDC}")
+	print(f"{Cores.FAIL}0. Sair{Cores.ENDC}")
+	opcaoa = int(input("X. Escolha uma opçao : "))
+	return opcaoa
+
+#Opções visualizadas pelo Professor
+def professor():
+  print(33 * '*')
+  print(f"{Cores.BOLD}{Cores.OKBLUE}***-- Perfil Professor --***{Cores.ENDC}")
+  print(33 * '*')
+  print(f"{Cores.BOLD}{Cores. OKGREEN}Tabela de escolha :{Cores.ENDC}")
+  print(f"{Cores.FAIL}1. Inserir avaliações{Cores.ENDC}")
+  print(f"{Cores.FAIL}2. Alterar avaliações{Cores.ENDC}")
+  print(f"{Cores.FAIL}3. Ver avaliações{Cores.ENDC}")
+  print(f"{Cores.FAIL}4. Excluir avaliações{Cores.ENDC}")
+  print(33 * '*')
+  print(f"{Cores.FAIL}5. Inserir Dados boletim{Cores.ENDC}")
+  print(f"{Cores.FAIL}6. Alterar dados boletim{Cores.ENDC}")
+  print(f"{Cores.FAIL}7. Ver dados boletim{Cores.ENDC}")
+  print(f"{Cores.FAIL}8. Excluir dados boletim{Cores.ENDC}")  
+  print(33 * '*')
+  print(f"{Cores.FAIL}0. Sair{Cores.ENDC}")
+  opcaop = int(input("X. Escolha uma opçao : "))
+  return opcaop
+
+# Exibição do menu principal
+def perfil():
+  print(33 * '*')
+  print(f"{Cores.BOLD}{Cores.OKBLUE}***-- Controle de Avaliação --**  {Cores.ENDC}")
+  print(33 * '*')
+  print(f"{Cores.FAIL}1. Perfil Professor{Cores.ENDC}")
+  print(f"{Cores.FAIL}2. Perfil Administrador{Cores.ENDC}")
+  print(f"{Cores.FAIL}3. Pesquisas Misc{Cores.ENDC}")
+  print(f"{Cores.FAIL}{Cores.FAIL}4. Sair{Cores.ENDC}")
+  print(19 * '*')
+  perfil = int(input('X. Selecione uma opção: '))
+  return perfil
+
+def exercicio():
+  print(33 * '*')
+  print(f"{Cores.BOLD}{Cores.OKBLUE}***-- Exercícios  --**  {Cores.ENDC}")
+  print(33 * '*')
+  print(f"{Cores.FAIL}1. Listar todas as tabelas{Cores.ENDC}")
+  print(f"{Cores.FAIL}2. Listar avaliações por periodo{Cores.ENDC}")
+  print(f"{Cores.FAIL}3. Listar avaliações por professor{Cores.ENDC}")
+  print(f"{Cores.FAIL}4. Listar avaliações por tipo{Cores.ENDC}")
+  print(f"{Cores.FAIL}5. Listar alunos sem avaliações{Cores.ENDC}")
+  print(f"{Cores.FAIL}6. Listar alunos por situação{Cores.ENDC}")
+  print(f"{Cores.FAIL}7. Listar alunos por notas{Cores.ENDC}")
+  print(33 * '*')
+  print(f"{Cores.FAIL}{Cores.FAIL}0. Sair{Cores.ENDC}")
+  print(19 * '*')
+  ex = int(input('X. Selecione uma opção: '))
+  return ex
+    
+def criar_conexao(banco):
+	"""Criando a conexão com o banco de dados Sqlite3."""
+	# Variável conn inicializada para estabelecer a conexão com o banco
+	conn = None
+	# Tentará fazer a conexão com o banco de dados
+	try: 
+		# Criando a conexão com o banco e salvando o objeto Connection na variável conn.
+		conn = sqlite3.connect(banco)
+		# Exibe a versão do Sqlite
+		print(sqlite3.sqlite_version)
+		# Sucesso na conexão. É retornado a conexão a quem chamou.
+		return conn
+		# Caso negativo, não conseguiu estabelecer conexão, retorna o erro que ocorreu.	
+	except Error as e: 
+		print(e)
+
+# Função para limpar a tela antes de exibir a próxima instrução
+def limpar():
+    # Importando o módulo do sistema operacional (os) para usar instruções dele
+    import os
+    # Importando o módulo time para utilizar o método sleep que aguarda o 
+    # tempo em segundos informando dentro do parênteses
+    
+    from time import sleep
+    
+    # Função para limpar a tela
+    def screen_clear():
+    # Para os sistemas operacionais: mac and linux(here, os.name is 'posix')
+        if os.name == 'posix':
+            _ = os.system('clear')
+        else:
+        # Para o sistema operacional windows
+            _ = os.system('cls')
+    # Aguarda 1 segundo para executar a próxima instrução
+  
+    # Chama a função de limpeza
+    screen_clear()
+
+if __name__ == '__main__':
+  limpar()
+  print(34 * '*')
+  print(f"{Cores.BOLD}{Cores.OKBLUE}***-- Controle de Avaliações --***  {Cores.ENDC}")
+  print(34 * '*')
+  print(f"\n{Cores.HEADER}{Cores.HEADER}Criando o banco de dados se não existir ... {Cores.ENDC}")
+
+  banco = input(f"{Cores.FAIL}{Cores.FAIL}Informe o Nome do Banco : {Cores.ENDC}")
+  print(f"{Cores.BOLD}{Cores.OKBLUE}Criando conexão ... {Cores.ENDC}")
+  conn = criar_conexao(banco)
+  print(f"{Cores.BOLD}{Cores.OKBLUE}\nCriando tabelas do banco de dados se não existirem ... {Cores.ENDC}")
+  schema.criar_tabelas(banco)
+
+  input(f"{Cores.BOLD}{Cores.OKBLUE}Pressione <ENTER> para prosseguir...{Cores.ENDC}")
+  limpar()
+
+	#Seleciona o Perfil do usuario 
+
+  opcaoperfil = perfil()
+  limpar()
+
+  while opcaoperfil != 4:
+    if opcaoperfil == 1:
+      limpar()
+      opcaoP = professor()	
+      while opcaoP != 0:
+        tabela = 'avaliacao'
+        if opcaoP == 1:
+          print(f'Inserir {tabela}')
+          tabelas.inserir(tabela, conn)
+        elif opcaoP == 2:
+          print(f'Atualizar {tabela}')
+          tabelas.atualizar(tabela, conn)
+        elif opcaoP == 3:
+          print(f'Pesquisar {tabela}')
+          tabelas.pesquisar(tabela, conn)
+        elif opcaoP == 4:
+          print(f'Excluir {tabela}')
+          tabelas.excluir(tabela, conn)
+
+        tabela = 'dataProva' #Dataprova
+        if opcaoP == 5:
+          print(f'Inserir {tabela}')
+          tabelas.inserir(tabela, conn)
+        elif opcaoP == 6:
+          print(f'Atualizar {tabela}')
+          tabelas.atualizar(tabela, conn)
+        elif opcaoP == 7:
+          print(f'Pesquisar {tabela}')
+          tabelas.pesquisar(tabela, conn)
+        elif opcaoP == 8:
+          print(f'Excluir {tabela}')
+          tabelas.excluir(tabela, conn)
+        else:
+          print('Opção inválida!')
+          sleep(1)
+        limpar()
+        opcaoP = professor()
+      else:
+        print("Obrigado professor!")
+        sleep(1)
+      limpar()
+      opcaoperfil = perfil()	
+
+ 		#alterar informações do professor
+    elif opcaoperfil == 2:
+      limpar()
+      opcaoadm = administrador()
+
+      while opcaoadm != 0:
+        tabela = 'professor'
+        if opcaoadm == 1:
+          print(f'Inserir {tabela}')
+          tabelas.inserir(tabela, conn)
+        elif opcaoadm == 2:
+          print(f'Atualizar {tabela}')
+          tabelas.atualizar(tabela, conn)
+        elif opcaoadm == 3:
+          print(f'Pesquisar {tabela}')
+          tabelas.pesquisar(tabela, conn)
+        elif opcaoadm == 4:
+          print(f'Excluir {tabela}')
+          tabelas.excluir(tabela, conn)
+
+        #Opções dentro da tabela aluno
+
+        tabela = 'aluno' 
+        if opcaoadm == 5:
+          print(f'Inserir {tabela}')
+          tabelas.inserir(tabela, conn)
+        elif opcaoadm == 6:
+          print(f'Atualizar {tabela}')
+          tabelas.atualizar(tabela, conn)
+        elif opcaoadm == 7:
+          print(f'Pesquisar {tabela}')
+          tabelas.pesquisar(tabela, conn)
+        elif opcaoadm == 8:
+          print(f'Excluir {tabela}')
+          tabelas.excluir(tabela, conn)
+
+#Opções dentro da tabela tipo, usado para provas
+
+        tabela = 'tipo'
+        if opcaoadm == 9:
+          tabela = 'tipo'
+          print(f'Inserir {tabela}')
+          tabelas.inserir(tabela, conn)
+        elif opcaoadm == 10:
+          tabela = 'tipo'
+          print(f'Atualizar {tabela}')
+          tabelas.atualizar(tabela, conn)
+        elif opcaoadm == 11:
+          tabela = 'tipo'
+          print(f'Pesquisar {tabela}')
+          tabelas.pesquisar(tabela, conn)
+        elif opcaoadm == 12:
+          tabela = 'tipo'
+          print(f'Excluir {tabela}')
+          tabelas.excluir(tabela, conn)
+        else: 
+          print("Opção inválida!")
+          sleep(1)
+        limpar()
+        opcaoadm = administrador()
+      else:
+        print("Obrigada administrador!")
+        sleep(1)
+      limpar()
+      opcaoperfil = perfil()
+
+  # Exercicios pedidos do trabalho
+    elif opcaoperfil == 3:
+      limpar()
+      opcaoEx = exercicio()
+      while opcaoEx != 0:
+        if opcaoEx == 1:
+          print(f'Mostrar todas as Tabelas')
+
+          print(f"{Cores.BOLD}{Cores.OKBLUE}Tabela Aluno{Cores.ENDC}")
+          tabela = 'aluno'
+          tabelas.pesquisar(tabela, conn)
+          print('\n')
+
+          print(f"{Cores.BOLD}{Cores.OKBLUE}Tabela Professor{Cores.ENDC}")
+          tabela = 'professor'
+          tabelas.pesquisar(tabela, conn)
+          print('\n')
+
+          print(f"{Cores.BOLD}{Cores.OKBLUE}Tabela Avaliação{Cores.ENDC}")
+          tabela = 'avaliacao'
+          tabelas.pesquisar(tabela, conn)
+          print('\n')
+
+          print(f"{Cores.BOLD}{Cores.OKBLUE}Tabela DataProva{Cores.ENDC}")
+          tabela = 'dataProva'
+          tabelas.pesquisar(tabela, conn)
+          print('\n')
+
+          print(f"{Cores.BOLD}{Cores.OKBLUE}Tabela Tipo{Cores.ENDC}")
+          tabela = 'tipo'
+          tabelas.pesquisar(tabela, conn)
+
+
+        elif opcaoEx == 2:
+          tabela = 'dataProva'
+          faixaPesq='periodo'
+          tabelas.pesquisaEsp(tabela, conn, faixaPesq)
+          print('\n')
+          
+        elif opcaoEx == 3:
+          tabela = 'dataProva'
+          faixaPesq='prof'
+          tabelas.pesquisaEsp(tabela, conn, faixaPesq)
+          print('\n')
+
+        elif opcaoEx == 4:
+            tabela = 'avaliacao'
+            faixaPesq='tipo'
+            tabelas.pesquisaEsp(tabela, conn, faixaPesq)
+            print('\n')
+
+        elif opcaoEx == 5:
+            tabela = 'aluno'
+            tabelas.listarSemAvaliacao(tabela, conn)
+            print('\n')
+        
+        elif opcaoEx == 6:
+            tabela = 'aluno'
+            tabelas.listarSituacao(tabela, conn)
+            print('\n')
+
+        elif opcaoEx == 7:
+            tabela = 'aluno'
+            tabelas.listarNotas(tabela, conn)
+            print('\n')
+        else:
+            print("Opção inválida!")
+            sleep(1)
+        limpar()
+        opcaoEx = exercicio()
+      else:
+        print("Nenhum exercicio encontrado! Use numeros de 1 a 7!")
+        sleep(1)
+      limpar()
+      opcaoperfil = perfil()
+      
+  else:
+    print('Volte sempre!')
---
+++ schema.py
@@ -0,0 +1,101 @@
+# Importando o módulo sqlite3 para criarmos nosso banco e manipulá-lo.
+import sqlite3
+from sqlite3 import Error
+from cores import Cores
+
+def criar_tabelas(banco):
+  # Definindo a conexão com o BD chamado 
+  conn = sqlite3.connect(banco)
+  # Definindo o cursor cuja função é permitir navegar e manipular os registros do BD.
+  c = conn.cursor()
+
+  try:
+    # Executando a instrução de criação da tabela aluno no BD
+    # a matrícula aponta para rowid (chave primaria da tabela)
+    c.execute("""CREATE TABLE IF NOT EXISTS aluno (
+        matricula INTEGER primary key,
+        nome TEXT,
+        cpf TEXT,
+        dataNasc DATE,
+        curso TEXT)""")
+
+    conn.commit()
+    
+    # Insere dados na tabela Aluno
+    #---------------------------------------------#
+    c.execute("INSERT INTO aluno VALUES ('123','Pedro','524534543','2000-11-11','ADS'),('456','joao','524556745','1998-11-11','ADS'),('789','Felipe','524533343','2004-01-02','ENG'),('112','Paulo','524533459','2007-08-09','ADS');")
+    conn.commit()
+   
+
+    # Executando a instrução de criação da tabela tipo
+    c.execute("""CREATE TABLE IF NOT EXISTS tipo (
+        idTipo INTEGER primary key,
+        descricao TEXT NOT NULL)
+    """)
+    conn.commit()
+
+     # Insere dados na tabela Tipo
+     #-------------------------------------------#
+    c.execute("INSERT INTO tipo VALUES ('1','P1'),('2','P2'),('3','IFA');")
+    conn.commit()
+    
+    # Executando a instrução de criação da tabela professor
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS professor (
+        idProf INTEGER primary key,
+        nome TEXT NOT NULL,
+        materia TEXT NOT NULL)
+    """)
+    conn.commit()	
+
+     # Insere dados na tabela professor
+     #-------------------------------------------#
+    c.execute("INSERT INTO professor VALUES ('1','Matarazo','ESW'),('2','Ana Banana','LP2'),('3','Felipe','SPO');")
+    conn.commit()
+
+
+    # Executando a instrução de criação da tabela avaliação
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS avaliacao (
+        idAvaliacao INTEGER primary key,
+        materia TEXT NOT NULL,
+        descricao TEXT NOT NULL,
+        notaMin DOUBLE NOT NULL,
+        notaMax DOUBLE NOT NULL,
+        notaAprov DOUBLE NOT NULL,
+        dataPrevista DATE NOT NULL,
+        idTipo INTEGER NOT NULL,
+        FOREIGN KEY (idTipo) REFERENCES tipo(idTipo))
+    """) 
+    conn.commit()
+
+     # Insere dados na tabela avaliação
+     #-------------------------------------------#
+    c.execute("INSERT INTO avaliacao VALUES ('1','ESW','Pres','4','10','6','2021-11-11','1'),('2','SPO','Pres','4','10','6','2021-10-10','2'),('3','ESW','Ead','4','10','6','2021-11-16','1');")
+    conn.commit()
+
+    # Executando a instrução de criação da tabela dataProva
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS dataProva (
+        idDataProva INTEGER primary key,
+        notaObt DOUBLE NOT NULL,
+        dataProva DATE NOT NULL,
+        materia TEXT NOT NULL, 
+        matricula INTEGER NOT NULL,
+        idProf INTEGER NOT NULL,
+        idAvaliacao INTEGER NOT NULL,
+        situacao TEXT NOT NULL,
+        FOREIGN KEY (matricula) REFERENCES aluno(matricula),
+        FOREIGN KEY (idProf) REFERENCES professor(idProf),
+        FOREIGN KEY (idAvaliacao) REFERENCES avaliacao(idAvaliacao))
+    """)
+    conn.commit()
+
+    # Insere dados na tabela dataProva
+    #-------------------------------------------#
+    c.execute("INSERT INTO dataProva VALUES ('1','8','2021-12-01','ESW','123','1','1','Aprovado'),('2','3','2021-11-16','SPO','456','3','2','Reprovado'),('3','8','2021-12-01','ESW','123','1','3','Aprovado'), ('4','5','2021-12-05','SPO','123','3','2','IFA');")
+    conn.commit()
+
+    print(f"{Cores.BOLD}{Cores.OKGREEN}Tabelas criadas com sucesso!{Cores.ENDC}")
+  except Error as e:
+    print(e)
